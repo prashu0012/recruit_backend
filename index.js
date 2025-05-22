b6f82ee8bcfad880517193f35fa765cbf9200a4c
Prashu Verma <prashuverma012@gmail.com>
2025-05-22 23:48:08 +0530

    temp cors

--- index.js
+++ index.js
@@ -15,10 +15,13 @@ import CompanyOverview from './src/models/companyOverviewModel.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//     credentials: true
+// }));
+
+// temp
+app.use(cors());
 
 app.use(express.json());
 app.use(bodyParser.json());
